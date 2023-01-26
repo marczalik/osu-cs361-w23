cfa451f1993706dbd0eb76f486b0821e2244352a
Marc Zalik <marc.zalik@gmail.com>
2023-01-26 08:17:40 -0500

    Add Background routing and results forms

--- chatdnd/src/routes/BackgroundCreator/+page.svelte
+++ chatdnd/src/routes/BackgroundCreator/+page.svelte
@@ -1,11 +1,16 @@
 <style>
-    button {
+    .button {
         background-color: #15616d;
         color: #ffecd1;
     }
 </style>
 
 <script>
+    import { enhance } from '$app/forms';
+
+    export let data;
+    export let form;
+
     let name = '';
     let race = '';
     let playerClass = '';
@@ -19,22 +24,43 @@
         
     }
 
-    function clear() {
-        
+    function onClear() {
+        name = '';
+        race = '';
+        playerClass = '';
+        gender = '';
+        homeland = '';
+        family = '';
+        adventureReason = '';
+        flaw = '';
     }
 </script>
 
-<form method="POST">
-    <input bind:value={name} placeholder="Name">
-    <input bind:value={race} placeholder="Race">
-    <input bind:value={playerClass} placeholder="Class">
-    <input bind:value={gender} placeholder="Gender">
-
-    <input bind:value={homeland} placeholder="Homeland">
-    <input bind:value={family} placeholder="Family Members">
-    <input bind:value={adventureReason} placeholder="Reason for Adventuring">
-    <input bind:value={flaw} placeholder="Flaw">
-
-    <button on:click={clear}>Clear Inputs</button>
-    <button on:click={send}>Submit</button>
+<br>
+{#if form?.error}
+	<p class="error">{form.error}</p>
+{:else if form?.name}
+<form method="POST" action="?/save" use:enhance>
+    <p>{form.name}</p>
+    <button name="save" id="save" class="button">Save</button>
 </form>
+{:else}
+<form method="POST" action="?/submit" use:enhance>
+    <input name="name" id="name" bind:value={name} placeholder="Name">
+    <input name="race" id="race" bind:value={race} placeholder="Race">
+    <input name="playerClass" id="playerClass" bind:value={playerClass} placeholder="Class">
+    <input name="gender" id="gender" bind:value={gender} placeholder="Gender">
+<br>
+<br>
+
+    <input name="homeland" id="homeland" bind:value={homeland} placeholder="Homeland">
+    <input name="family" id="family" bind:value={family} placeholder="Family Members">
+    <input name="adventureReason" id="adventureReason" bind:value={adventureReason} placeholder="Reason for Adventuring">
+    <input name="flaw" id="flaw" bind:value={flaw} placeholder="Flaw">
+<br>
+<br>
+
+    <input type="button" name="clear" id="clear" class="button" on:click={onClear} value="Clear Inputs"/>
+    <button name="submit" id="submit" class="button">Submit</button>
+</form>
+{/if}
--- chatdnd/src/routes/NameSuggester/+page.svelte
+++ chatdnd/src/routes/NameSuggester/+page.svelte
@@ -7,6 +7,7 @@
 
 <script>
     import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
 
     export let data;
     export let form;
@@ -24,16 +25,22 @@
     }
 </script>
 
+<br>
 {#if form?.error}
 	<p class="error">{form.error}</p>
 {:else if form?.name}
+<form method="POST" action="?/save" use:enhance>
     <p>{form.name}</p>
+    <button name="save" id="save" class="button">Save</button>
+</form>
 {:else}
 <form method="POST" action="?/submit" use:enhance>
     <input name="name" id="name" bind:value={name} placeholder="Name">
     <input name="race" id="race" bind:value={race} placeholder="Race">
     <input name="playerClass" id="playerClass" bind:value={playerClass} placeholder="Class">
     <input name="gender" id="gender" bind:value={gender} placeholder="Gender">
+<br>
+<br>
 
     <input type="button" name="clear" id="clear" class="button" on:click={onClear} value="Clear Inputs"/>
     <button name="submit" id="submit" class="button">Submit</button>
